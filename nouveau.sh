ff529d76b9b09316be57da4771cf5a0e3e937ed4
Sergey Kovalenko <seryoga.engineering@gmail.com>
2019-01-14 20:14:35 +0200

    Minor changes

--- nouveau.sh
+++ nouveau.sh
@@ -2,8 +2,9 @@
 sudo mkdir /etc/X11/xorg.conf.d
 cat << 'EOF' | sudo tee -a /etc/X11/xorg.conf.d/20-nouveau.conf
 Section "Device"
-  Identifier "nvidia"
+  Identifier "Nvidia"
   Driver     "nouveau"
   Option     "GLXVBlank" "true"
+  Option     "DRI" "3"
 EndSection
 EOF
